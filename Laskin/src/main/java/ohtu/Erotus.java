721b215860bc75396af1ca8761db0a02156836b3
MJL7068 <mattil@netti.fi>
2017-04-27 09:39:20 +0300

    viikko 6 tehtävä 2 tehty

--- Laskin/src/main/java/ohtu/Erotus.java
+++ Laskin/src/main/java/ohtu/Erotus.java
@@ -3,26 +3,28 @@ package ohtu;
 import javax.swing.JTextField;
 
 public class Erotus implements Komento {
-
     private Sovelluslogiikka sovellus;
     private JTextField tuloskentta;
     private JTextField syotekentta;
+    private int edellinen;
 
     public Erotus(Sovelluslogiikka sovellus, JTextField tuloskentta, JTextField syotekentta) {
         this.sovellus = sovellus;
         this.tuloskentta = tuloskentta;
         this.syotekentta = syotekentta;
+        this.edellinen = 0;
     }
 
     @Override
     public void suorita() {
+        edellinen = Integer.parseInt(tuloskentta.getText());
         sovellus.miinus(Integer.parseInt(syotekentta.getText()));
         tuloskentta.setText("" + sovellus.tulos());
     }
 
     @Override
     public void peru() {
-        
+        tuloskentta.setText("" + edellinen);
     }
 
 }
--- Laskin/src/main/java/ohtu/Summa.java
+++ Laskin/src/main/java/ohtu/Summa.java
@@ -6,22 +6,25 @@ public class Summa implements Komento {
     private Sovelluslogiikka sovellus;
     private JTextField tuloskentta;
     private JTextField syotekentta;
+    private int edellinen;
 
     public Summa(Sovelluslogiikka sovellus, JTextField tuloskentta, JTextField syotekentta) {
         this.sovellus = sovellus;
         this.tuloskentta = tuloskentta;
         this.syotekentta = syotekentta;
+        this.edellinen = 0;
     }
 
     @Override
     public void suorita() {
+        edellinen = Integer.parseInt(tuloskentta.getText());
         sovellus.plus(Integer.parseInt(syotekentta.getText()));
         tuloskentta.setText("" + sovellus.tulos());
     }
 
     @Override
     public void peru() {
-
+        tuloskentta.setText("" + edellinen);
     }
     
 }
